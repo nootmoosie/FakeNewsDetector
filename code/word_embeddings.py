76f5d86ba76fc3800a911bb171bbd749a2f3cdc9
Nathan Masi <nmasi@umass.edu>
2018-11-12 18:04:28 -0500

    Added test word embedding file

---
+++ code/word_embeddings.py
@@ -0,0 +1,8 @@
+import gensim
+
+# Load Google's pre-trained Word2Vec model.
+model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
+
+
+print(model['hello'].shape)
+# print(model.most_similar(positive=['woman', 'king'], negative=['man']))
