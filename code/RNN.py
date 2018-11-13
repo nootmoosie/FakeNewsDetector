8911ea6a1bfda04e6827a4cad1cd62436dad4214
Nathan Masi <nmasi@umass.edu>
2018-11-13 16:03:19 -0500

    progress on processing data, start of Nate's TF RNN implementation.

---
+++ code/RNN.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+import gensim
+import numpy as np
+
+from tensorflow.contrib import rnn
+from process_data import process_csv_data
+
+
+# Load Google's pre-trained Word2Vec model.
+word_embeddings = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
+
+train_data, test_data = process_csv_data('../data/train.csv', '../data/test.csv')
+print(np.array(train_data).shape)
+print(np.array(test_data).shape)
+exit()
+
+hm_epochs = 3
+n_classes = 2
+batch_size = 128
+
+chunk_size = 28
+n_chunks = 32
+rnn_size = 128
+
+x = tf.placeholder('float', [None, n_chunks, chunk_size])
+y = tf.placeholder('float')
+
+
+def recurrent_neural_network(x):
+    layer = {'weights': tf.Variable(tf.random_normal([rnn_size, n_classes])),
+             'biases': tf.Variable(tf.random_normal([n_classes]))}
+
+    x = tf.transpose(x, [1, 0, 2])  # changes dimensionality to comply w/ rnn_cell API
+    x = tf.reshape(x, [-1, chunk_size])
+    x = tf.split(x, n_chunks, 0)
+
+    lstm_cell = rnn.BasicLSTMCell(rnn_size)
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+
+    output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']  # final output multiplied with weights + biases
+
+    return output
+
+
+def train_neural_network(x):
+    prediction = recurrent_neural_network(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    with tf.Session() as sess:
+        # OLD:
+        # sess.run(tf.initialize_all_variables())
+        # NEW:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                # print("Reshaping input into chunks:")
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                # print(tf.shape(epoch_x))
+                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+                # print(tf.shape(epoch_x))
+                # print("DONE.")
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',
+              accuracy.eval({x: mnist.test.images.reshape((-1, n_chunks, chunk_size)), y: mnist.test.labels}))
+
+
+train_neural_network(x)
+
+tf.logging.set_verbosity(old_v)
--- code/process_data.py
+++ code/process_data.py
@@ -1,5 +1,7 @@
 import os
+import csv
 import numpy as np
+
 
 def process_csv_data(train_path, test_path):
     train_data = []
@@ -8,24 +10,56 @@ def process_csv_data(train_path, test_path):
     train_file = open(train_path, 'r', encoding='utf8')
     test_file = open(test_path, 'r', encoding='utf8')
 
-    for article in train_file:
-        train_data.append(article)
+    train_str = train_file.read().splitlines()
+    test_str = test_file.read().splitlines()
 
-    for article in test_file:
-        test_data.append(article)
+
+    # SASHAS:
+    # for data_point in train_str:
+    #     line = csv.reader([data_point], quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    #     entry = list(i for i in line)
+    #     print(entry)
+    #     #print('id: ', entry[0])
+    #     #print('title: ', entry[1])
+    #     #print('author: ', entry[2])
+    #     #print('text: ', entry[3])
+    #     #print('label: ', entry[4])
+    #     #train_data.append(entry)
+
+    # print(type(train_str))
+    # batch = train_str[:3]
+    # print(batch)
+    # NATES:
+
+    for entry in csv.reader(train_str, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True):
+        print('id: ', entry[0])
+        print('title: ', entry[1])
+        print('author: ', entry[2])
+        print('text: ', entry[3])
+        print('label: ', entry[4])
+        train_data.append(entry)
+
+    for entry in csv.reader(test_str, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True):
+        # print('id: ', entry[0])
+        # print('title: ', entry[1])
+        # print('author: ', entry[2])
+        # print('text: ', entry[3])
+        # print('label: ', entry[4])
+        test_data.append(entry)
+
 
     return train_data, test_data
 
-def withhold_data(training_data, percentage):
-	train_size = int(percentage * len(training_data))
-	train_data = training_data[:train_size]
-	withheld_data = training_data[train_size:]
 
-	return train_data, withheld_data
+def withhold_data(training_data, percentage):
+    train_size = int(percentage * len(training_data))
+    train_data = training_data[:train_size]
+    withheld_data = training_data[train_size:]
+
+    return train_data, withheld_data
 
 
 train_data, test_data = process_csv_data('../data/train.csv', '../data/test.csv')
 
-print('train data format: ' + train_data[0])
-print('test data format: ' + test_data[0])
+
 
